2b5fb1b40fc67dbef52d2eebec9df77beade7780
Noah Paessel <knowuh@gmail.com>
2009-09-14 18:19:52 -0400

    Install java and ant and maven.

--- java.sh
+++ java.sh
@@ -8,10 +8,23 @@ source $BASE_DIR/base.sh
 # install java from rpm:
 ###########################################
 sudo setenforce 0
-sudo yum install java-1.6.0-openjdk java-1.6.0-openjdk-devel
+sudo yum install -y java-1.6.0-openjdk java-1.6.0-openjdk-devel
+
 
 ###########################################
-# ant from source java from rpm:
+# set JAVA_HOME and export
+###########################################
+JAVA_HOME=/usr/lib/jvm/jre/
+export JAVA_HOME
+if grep JAVA_HOME /etc/skel/.bash_profile ; then 
+  sudo echo "JAVA_HOME already set in .bash_profile"
+else
+  sudo echo "JAVA_HOME=$JAVA_HOME" >> /etc/skel/.bash_profile;
+  sudo echo "export JAVA_HOME" >> /etc/skel/.bash_profile;
+fi
+
+###########################################
+# ant from binary distro:
 ###########################################
 ANT_VERSION=1.7.1
 ANT_TARBALL=apache-ant-$ANT_VERSION-bin.tar.gz
@@ -37,23 +50,29 @@ fi
 sudo ln -s $ANT_DEST/bin/ant $ANT_BIN
 
 
-# install jruby from github sources
-JRUBY_HOME=$SRC_DIR/jruby
-
-# jruby source
-if [ -e $JRUBY_HOME ]; then
-  echo "found $JRUBY_HOME, checkign for git updates"
-  cd $JRUBY_HOME
-  git pull
+###########################################
+# maven binary distro:
+###########################################
+# http://mirrors.kahuki.com/apache/maven/binaries/apache-maven-2.2.1-bin.tar.gz
+MVN_VERSION=2.2.1
+MVN_TARBALL=apache-maven-$MVN_VERSION-bin.tar.gz
+MVN_DEST=$SRC_DIR/apache-maven-$MVN_VERSION
+MVN_BIN=$BIN_DIR/mvn
+MVN_DOWNLOAD=http://mirrors.kahuki.com/apache/maven/binaries/$MVN_TARBALL
+cd $SRC_DIR
+if [ -e $SRC_DIR/$MVN_TARBALL ]; then
+  echo "$MVN_TARBALL found"
 else
-  cd $SRC_DIR
-  git clone git://github.com/jruby/jruby.git $JRUBY_HOME
+  wget $MVN_DOWNLOAD
 fi
 
-# build
-cd $JRUBY_HOME
-ant
+if [ -e $MVN_DEST ]; then
+  rm -rf $MVN_DEST
+fi
+tar -zxf $MVN_TARBALL
+cd $MVN_DEST
 
-# smylink binaries
-
-
+if [ -e $MVN_BIN ]; then
+ sudo rm $MVN_BIN
+fi
+sudo ln -f -s $MVN_DEST/bin/mvn $MVN_BIN
