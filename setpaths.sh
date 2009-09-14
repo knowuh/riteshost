a0b1722a7c520d15d9d99a37caf008af96e8d973
Noah Paessel <knowuh@gmail.com>
2009-09-14 18:18:28 -0400

    Setup the paths required for the rites setup.

--- setpaths.sh
+++ setpaths.sh
@@ -3,8 +3,15 @@
 #######################################
 # make the shared source tree if needed
 #######################################
-SRC_DIR=/usr/local/src
-BIN_DIR=/usr/local/bin
+PREFIX_DIR=/usr/local
+SRC_DIR=$PREFIX_DIR/src
+BIN_DIR=$PREFIX_DIR/bin
+
 sudo mkdir -p $SRC_DIR
-sudo chown -R $USER:users $SRC_DIR
+sudo chown -R ${USER}:users $SRC_DIR
 sudo chmod -R g+w $SRC_DIR
+
+# crazy, but make sure that the user can write to files in their home directoy
+# because some of the previous runs might have messed up .gemrc &etc.
+cd ~
+sudo chown -R $USER $HOME
