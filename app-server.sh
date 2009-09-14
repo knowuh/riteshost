7a0217eac09f42f46db159d768d655580d370de2
Noah Paessel <knowuh@gmail.com>
2009-09-14 18:20:41 -0400

    Install rails, jruby, passenger, apache httpd &etc.

--- app-server.sh
+++ app-server.sh
@@ -5,7 +5,48 @@ source $BASE_DIR/mysql.sh
 source $BASE_DIR/ruby.sh
 source $BASE_DIR/apache.sh
 source $BASE_DIR/rails.sh
+source $BASE_DIR/jruby.sh
 
+#########################################################
+# install required c++ library (needed for passenger)
+#########################################################
+sudo yum install -y gcc-c++ 
 
-# we probably need to pass some arguments here:
+#########################################################
+# configure web-root directory
+#########################################################
+WEB_ROOT=/web
+RITES_ROOT=$WEB_ROOT/rites
+GIT_SOURCE=git://github.com/stepheneb/rigse.git
+sudo mkdir -p $RITES_ROOT
+sudo chown -R $USER:users $RITES_ROOT
+
+#########################################################
+# install rails and passenger gems
+#########################################################
 sudo gem install rails mysql passenger 
+
+#########################################################
+# run passenger setup
+#########################################################
+cd
+sudo -E passenger-install-apache2-module -a
+
+#########################################################
+# pull some version from github 
+# (later we will just cap deploy xxxx where xxx is here)
+#########################################################
+cd $RITES_ROOT
+rm -rf git clone $GIT_SOURCE $RITES_ROOT/current
+git clone $GIT_SOURCE $RITES_ROOT/current
+cd $RITES_ROOT/current
+git co origin/staging
+
+#########################################################
+# configure web-server
+#########################################################
+# Please edit your Apache configuration file, and add these lines:
+# 
+#    LoadModule passenger_module /usr/local/lib/ruby/gems/1.8/gems/passenger-2.2.5/ext/apache2/mod_passenger.so
+#    PassengerRoot /usr/local/lib/ruby/gems/1.8/gems/passenger-2.2.5
+#    PassengerRuby /usr/local/bin/ruby
