21ed9884cbe0e6ecddb9d86b42ab80748b669efa
Noah Paessel <knowuh@gmail.com>
2009-09-14 18:19:27 -0400

    setup and install rails.

--- rails.sh
+++ rails.sh
@@ -1,1 +1,8 @@
-source $BASE_DIR/base.sh
+#!/usr/bin/env bash
+BASE_DIR=~/rites_host
+cd $BASE_DIR
+source $BASE_DIR/setpaths.sh
+
+sudo gem install --no-ri --no-rdoc rails mysql mongrel capistrano capistrano-ext chriseppstein-compass has_many_polymorphs haml passenger
+
+# probably other gems would be good too
